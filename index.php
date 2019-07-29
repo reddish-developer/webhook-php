3f37d12898b2a9a0389664a21d973ed0439c75b8
Junior <reddish.developer@hotmail.com>
2019-07-29 18:07:19 -0500

    segundo cambio

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-// Process only when method is POST
+// Process only juniorwhen method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
