bdb690afb520765558a095e28c246ee1cfa28c54
Junior <reddish.developer@hotmail.com>
2019-07-29 18:58:00 -0500

    sin echo

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
-echo $json ;	$text = $json->result->parameters->text;
+$text = $json->result->parameters->text;
 
 	switch ($text) {
 		case 'hi':
