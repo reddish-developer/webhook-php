2ae0f845d8aa790d854a6ab8f52bd1135eaaf551
reddish-developer <reddish.developer@hotmail.com>
2019-07-31 13:54:41 -0500

    segunda prueba

--- index.php
+++ index.php
@@ -42,18 +42,77 @@ $response->source = "webhook";
 	
 
 $response = array (
-  'fulfillmentText' => 'displayed&spoken response',
+  'fulfillmentText' => 'This is a text response',
   'fulfillmentMessages' => 
   array (
     0 => 
     array (
-      'text' => 
+      'card' => 
       array (
-        0 => $speech,
+        'title' => 'card title',
+        'subtitle' => 'card text',
+        'imageUri' => 'https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png',
+        'buttons' => 
+        array (
+          0 => 
+          array (
+            'text' => 'button text',
+            'postback' => 'https://assistant.google.com/',
+          ),
+        ),
       ),
     ),
   ),
   'source' => 'webhook',
+  'payload' => 
+  array (
+    'google' => 
+    array (
+      'expectUserResponse' => true,
+      'richResponse' => 
+      array (
+        'items' => 
+        array (
+          0 => 
+          array (
+            'simpleResponse' => 
+            array (
+              'textToSpeech' => 'this is a simple response',
+            ),
+          ),
+        ),
+      ),
+    ),
+    'facebook' => 
+    array (
+      'text' => 'Hello, Facebook!',
+    ),
+    'slack' => 
+    array (
+      'text' => 'This is a text response for Slack.',
+    ),
+  ),
+  'outputContexts' => 
+  array (
+    0 => 
+    array (
+      'name' => 'projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name',
+      'lifespanCount' => 5,
+      'parameters' => 
+      array (
+        'param' => 'param value',
+      ),
+    ),
+  ),
+  'followupEventInput' => 
+  array (
+    'name' => 'event name',
+    'languageCode' => 'en-US',
+    'parameters' => 
+    array (
+      'param' => 'param value',
+    ),
+  ),
 );
 
 echo json_encode($response);
