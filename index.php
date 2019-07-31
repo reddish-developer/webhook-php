0e5f6ed55d5aa312e13f71cd3f23bb99e69639e4
reddish-developer <reddish.developer@hotmail.com>
2019-07-31 14:04:21 -0500

    V2 RESPONSE GENERAL

--- index.php
+++ index.php
@@ -42,28 +42,18 @@ $response->source = "webhook";
 	
 
 $response = array (
-  'fulfillmentText' => 'This is a text response',
+  'fulfillmentText' => 'displayed&spoken response',
   'fulfillmentMessages' => 
   array (
     0 => 
     array (
-      'card' => 
+      'text' => 
       array (
-        'title' => 'card title',
-        'subtitle' => 'card text',
-        'imageUri' => 'https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png',
-        'buttons' => 
-        array (
-          0 => 
-          array (
-            'text' => 'button text',
-            'postback' => 'https://assistant.google.com/',
-          ),
-        ),
+        0 => 'text response',
       ),
     ),
   ),
-  'source' => 'webhook',
+  'source' => 'example.com',
   'payload' => 
   array (
     'google' => 
